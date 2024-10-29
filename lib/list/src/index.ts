6436e43f80e0fbd8c453c8acff5ff7ae622b50db
soetas <3228891558@qq.com>
2024-10-29 22:08:11 +0800

    refactor: improve project structure

---
+++ lib/list/src/index.ts
@@ -0,0 +1,4 @@
+export { default as ArrayList } from './arrayList'
+export { default as LinkedList } from './linkedList'
+export { default as DoublyLinkedList} from './doublyLinkedList'
+
---
+++ scripts/build.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env ts-node
+const path = require('node:path')
+const fs = require('node:fs/promises')
+const rollup = require('rollup')
+
+
